fb21ae226b15b9b79da624e4b07e0dcd13a4bfe7
Vikesh raut <Vikeshraut952@gmail.com>
2024-12-07 18:17:33 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,9 +26,9 @@ function Home() {
           <span>Developer</span>
         </span>
 
-        <div className='flex justify-evenly flex-row md:gap-1'>
-          <NavLink className='flex flex-row items-center justify-center bg-gray-50 rounded-lg h-10 w-28 ml-8 font-bold text-slate-950 gap-2' to="https://github.com/Vikesh115" target='none'><FaGithub />Github</NavLink>
-          <NavLink className='flex flex-row items-center justify-center bg-gray-50 rounded-lg h-10 w-28 ml-8 font-bold text-slate-950 gap-2' to='https://www.linkedin.com/in/vikesh-raut/' target='none'><FaLinkedin />LinkedIn</NavLink>
+        <div className='flex flex-row gap-2'>
+          <NavLink className='flex flex-row items-center justify-center bg-gray-50 rounded-lg h-10 w-28 font-bold text-slate-950 gap-2' to="https://github.com/Vikesh115" target='none'><FaGithub />Github</NavLink>
+          <NavLink className='flex flex-row items-center justify-center bg-gray-50 rounded-lg h-10 w-28 font-bold text-slate-950 gap-2' to='https://www.linkedin.com/in/vikesh-raut/' target='none'><FaLinkedin />LinkedIn</NavLink>
         </div>
 
       </div>
