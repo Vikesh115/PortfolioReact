1476ac9e1f7c55319b15031d6804d31e9c8c805a
Vikesh115 <vikeshraut952@gmail.com>
2025-01-16 21:33:16 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function AboutTest() {
   return (
-    <div className='md:flex bg-blue-500 w-[100%] gap-3 justify-center items-center h-screen'>
+    <div className='md:flex bg-blue-500 w-[100%] gap-3 justify-center items-center md:h-screen'>
       <div className="flex justify-center md:pt-5 md:mt-0 md:w-[40%] w-[100%]">
-        <div className="flex flex-col flex-wrap bg-gradient-to-r from-indigo-500 via-lime-500 to-cyan-500 text-white font-bold p-4 rounded-lg shadow-2xl w-[100%]">
+        <div className="flex flex-col flex-wrap bg-gradient-to-r from-indigo-500 via-lime-500 to-cyan-500 text-white font-bold p-4 md:rounded-lg shadow-2xl w-[100%]">
             <div className='flex justify-center font-bold text-2xl'>About ME</div>
             <div className='flex pt-10'>
             Web Developer with expertise of React.js, Redux, HTML and CSS. currently learning Full stack development.
@@ -15,8 +15,8 @@ function AboutTest() {
             </div>
         </div>
       </div>
-      <div className='md:flex hidden md:w-[40%] w-[100%] justify-center'>
-        <img src="https://assets-v2.lottiefiles.com/a/c3a6dd4c-117e-11ee-9c3a-7f367549e774/a9hBSeOLNx.gif" alt="" className='flex rounded-2xl'/>
+      <div className='flex md:w-[40%] w-[100%] justify-center'>
+        <img src="https://assets-v2.lottiefiles.com/a/c3a6dd4c-117e-11ee-9c3a-7f367549e774/a9hBSeOLNx.gif" alt="" className='flex md:rounded-2xl'/>
       </div>
     </div>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,37 +8,37 @@ function Projects() {
     <div className='flex w-[100%]  items-center '>
       <div className='flex flex-col flex-wrap  items-center gap-2 md:gap-8 w-[100%] bg-indigo-500 p-4'>
 
-        <span className='flex font-bold text-2xl text-white pt-20'>Projects</span>
+        <span className='flex font-bold text-2xl text-white md:pt-20'>Projects</span>
 
         <div className='flex w-[90%] md:flex-nowrap flex-wrap-reverse justify-center'>
-          <div className='flex rounded-l-lg bg-gradient-to-r from-stone-500 via-purple-500 to-teal-50 flex-col w-[100%] md:w-[100%] gap-2 justify-center items-center'>
+          <div className='flex md:rounded-l-lg bg-gradient-to-r from-stone-500 via-purple-500 to-teal-50 flex-col w-[100%] md:w-[100%] gap-2 justify-center items-center'>
             <span className='flex text-xl gap-2 items-center justify-center'>
               <span className='flex text-2xl'>Food delivery app</span>
-              <NavLink className='flex' to="https://github.com/Vikesh115" target='none'><FaExternalLinkAlt size={20} /></NavLink>
+              <NavLink className='flex' to="https://food-app-one-ruddy.vercel.app/signup" target='none'><FaExternalLinkAlt size={20} /></NavLink>
             </span>
-            <span className='flex md:flex-row flex-wrap gap-2 md:gap-6 justify-center'>
+            <span className='flex md:flex-row flex-wrap gap-2 md:gap-6 justify-center p-1'>
               {project1.map((skill) => (
-                <span key={skill.id} className='flex text-black font-semibold bg-white rounded-md p-1 h-6 items-center justify-center'>{skill.name}</span>
+                <span key={skill.id} className='flex text-black font-semibold bg-white rounded-md p-2  items-center justify-center'>{skill.name}</span>
               ))}
             </span>
           </div>
-          <div className='flex rounded'>
-            <img src="https://images.pexels.com/photos/29306497/pexels-photo-29306497/free-photo-of-delicious-doner-kebab-with-fries-and-cola.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food" className='flex rounded-r-lg' />
+          <div className='flex md:rounded'>
+            <img src="https://images.pexels.com/photos/29306497/pexels-photo-29306497/free-photo-of-delicious-doner-kebab-with-fries-and-cola.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food" className='flex md:rounded-r-lg' />
           </div>
         </div>
 
         <div className='flex w-[90%] flex-wrap md:flex-nowrap  justify-center '>
           <div className='flex '>
-            <img src="https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food" className='flex rounded-l-lg'/>
+            <img src="https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food" className='flex md:rounded-l-lg'/>
           </div>
-          <div className='flex bg-gradient-to-r from-purple-500 via-sky-500 to-green-500 flex-col w-[100%] md:w-[100%] gap-2 justify-center items-center rounded-r-lg'>
+          <div className='flex bg-gradient-to-r from-purple-500 via-sky-500 to-green-500 flex-col w-[100%] md:w-[100%] gap-2 justify-center items-center md:rounded-r-lg'>
             <span className='flex text-xl gap-2 items-center justify-center'>
               <span className='flex text-2xl'>GET Youtube Subscriber</span>
               <span className='flex'><NavLink to="https://github.com/Vikesh115" target='none'><FaExternalLinkAlt size={20} /></NavLink></span>
             </span>
-            <div className='flex flex-row flex-wrap gap-2 md:gap-6 justify-center'>
+            <div className='flex flex-row flex-wrap gap-2 md:gap-6 justify-center p-1'>
               {project2.map((skill) => (
-                <span key={skill.id} className='flex text-black font-semibold bg-white rounded-md p-1 h-6 items-center justify-center'>{skill.name}</span>
+                <span key={skill.id} className='flex text-black font-semibold bg-white rounded-md p-2 items-center justify-center'>{skill.name}</span>
               ))}
             </div>
           </div>
