85c95be00f2428c3d26c1356448ef531652c9933
Vikesh115 <vikeshraut952@gmail.com>
2025-03-21 09:51:28 +0530

    Footer changed

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,7 +5,7 @@ import { FaGithubSquare, FaLinkedinIn } from "react-icons/fa";
 function Footer() {
   return (
     <div className='flex w-[100%] justify-center items-center bg-white'>
-      <div className='flex flex-wrap flex-col justify-center items-center gap-3 md:gap-6 w-[100%] bg-gradient-to-r from-indigo-400 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-white p-2'>
+      <div className='flex flex-wrap flex-col justify-center items-center gap-3 md:gap-6 w-[100%] bg-black text-white p-2'>
         <span className='flex justify-center md:text-2xl'>Contact</span>
         <form action="post" className='flex flex-col justify-center items-center max-w-[70%] '>
           <input type="text" name="name" placeholder='name' id="" className='flex p-2 rounded-2xl'/>
